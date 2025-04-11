e960ed7c7a63ad0b22a26ce6ec18af1a835b9e32
Mohit Sahu <131155860+Mohit-1221@users.noreply.github.com>
2025-04-11 05:08:16 +0000

    conversion ex

---
+++ js-work/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 2); // we are using nodejs not browser
+
+let name = "Mohit"
+let age = 23
+let isLoggedIn = true;
+
+// number => 2 to power 53
+// bigInt
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+ 
+console.log(typeof "Mohit"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ js-work/03_conversionOperation.js
@@ -0,0 +1,88 @@
+let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let  valueInNumber = Number(score);  
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN(not a number)
+// null => 0
+// true => 1, false  => 0
+
+// let isLoggegIn = 1;
+
+// let booleanLoggedIn = Boolean(isLoggegIn);
+// console.log(booleanLoggedIn);
+
+// 1 => true  0 => false
+// "" => false
+// "mohit" => true
+
+// let someNumber = 22
+
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/************************************Operations**************************/
+// let num = 1;
+// let negNum = -num;
+// console.log(negNum);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2/3); // 0.6666666666666666
+// console.log(2*3);
+// console.log(2**3); // 2^3 = 8
+// console.log(2%3);
+
+// let str1 = "Hello";
+// let str2 = " Mohit";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(2 + "1"); // string concatenate
+// console.log("2" + 1); // string concatenate
+// let d = 5 - "1" // normal difference
+// console.log(d);
+//console.log(2 + 3 + "4"); // number to number => addition, number to string => string concatenation
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+// console.log(+"m"); // NAN
+
+// let x = 3;
+// const y = x++; // first return the value then increement (Postfix)
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a; // first increement the value then return(Prefix)
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+// console.log(null > 0);
+// console.log(null >= 0); // true
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null <= 0); // true
+
+// console.log(undefined > 0); // always false
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// == this checks only value if different data type then change in that value 
+// console.log(2 == 2); // true
+// console.log("2" == 2); // true
+// console.log("1" == 2); // false
+// console.log("2" === 2); // false
+// === this checks both type of data and then value
+
+
+
+
